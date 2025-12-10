141b5b064d58ecec7fcd7067417d8d33c419b477
Tunegj <tongje04878@stud.noroff.no>
2025-12-10 16:09:45 +0100

    added basic styling to project cards, needs tweaking

--- js/data/projects.js
+++ js/data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "RainyDays",
     description:
       "Jacket e-commerce website built with HTML & CSS - Cross course assignment for Noroff", //remember to change if adding JS
-    image: "placeholder.jpg", //remember to add photo
+    image: "./assets/thumbnail/8-perfume-pink-candy.jpg", //remember to add photo
     github: "https://github.com/Tunegj/Html-CSS-ToneGjerde",
     live: "https://tunegj.github.io/Html-CSS-ToneGjerde/",
   },
@@ -13,7 +13,7 @@ export const projects = [
     title: "CommunityScienceMuseum",
     description:
       "Museum website built with HTML and CSS - Semester project for Noroff",
-    image: "placeholder.jpg", //remember to add photo
+    image: "./assets/thumbnail/8-perfume-pink-candy.jpg", //remember to add photo
     github: "https://github.com/Tunegj/TonesSemesterProject",
     live: "https://tunegj.github.io/TonesSemesterProject/",
   },
@@ -22,7 +22,7 @@ export const projects = [
     title: "SquareEyes",
     description:
       "Website for bying/renting movies built with HTML, CSS and JavaScript - JS1 course assignment for Noroff",
-    image: "placeholder.jpg", //remember to add photo
+    image: "./assets/thumbnail/8-perfume-pink-candy.jpg", //remember to add photo
     github: "https://github.com/Tunegj/Square.Eyes",
     live: "https://tunegj.github.io/Square.Eyes/",
   },
@@ -31,7 +31,7 @@ export const projects = [
     title: "Mirae",
     description:
       "Fictional e-commerce website built with HTML, CSS and JavaScript - Project Exam Noroff",
-    image: "placeholder.jpg", //remember to add photo
+    image: "./assets/thumbnail/8-perfume-pink-candy.jpg", //remember to add photo
     github: "https://github.com/Tunegj/Project-Exam-Tone",
     live: "https://tunegj.github.io/Project-Exam-Tone/",
   },
